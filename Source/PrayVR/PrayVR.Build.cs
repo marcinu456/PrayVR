bd8197bb88de098bb005da721d0d7a64f2d39972
Marcin <marcinu456@gmail.com>
2023-06-14 15:56:29 +0200

    Update To UE 5.2

--- Source/PrayVR/PrayVR.Build.cs
+++ Source/PrayVR/PrayVR.Build.cs
@@ -8,7 +8,7 @@ public class PrayVR : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem", "HeadMountedDisplay", "GeometricObjects", "UMG" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem", "HeadMountedDisplay", "UMG" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
